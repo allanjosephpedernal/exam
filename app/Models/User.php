777dfe32f1f51bb3b17beada4ef14f0235400d8b
allanjosephpedernal <allanjosephpedernal@gmail.com>
2021-10-18 17:34:09 +0800

    enhance user model fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'user_name',
+        'avatar',
         'email',
         'password',
+        'registered_at'
     ];
 
     /**
